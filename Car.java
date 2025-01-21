15ed411bfde7d0902ef5b3fcf85cb5f4d46559dd
Shrike05 <2005cookedcarrots@gmail.com>
2025-01-21 09:34:28 +0100

    Uppgift 1

---
+++ Car.java
@@ -0,0 +1,56 @@
+import java.awt.*;
+
+public class Car {
+    protected int nrDoors; // Number of doors on the car
+    protected double enginePower; // Engine power of the car
+    protected double currentSpeed; // The current speed of the car
+    protected Color color; // Color of the car
+    public String modelName; // The car model name
+    
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+	    color = clr;
+    }
+
+    public void startEngine(){
+	    currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+	    currentSpeed = 0;
+    }
+    
+    public double speedFactor(){
+        return 0;
+    }
+
+    public void incrementSpeed(double amount){
+    }
+
+    public void decrementSpeed(double amount){
+    }
+    
+    // TODO fix this method according to lab pm
+    public void gas(double amount){
+        incrementSpeed(amount);
+    }
+
+    // TODO fix this method according to lab pm
+    public void brake(double amount){
+        decrementSpeed(amount);
+    }
+}
